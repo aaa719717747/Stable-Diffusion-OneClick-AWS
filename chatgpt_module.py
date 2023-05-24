8578c0a4b00402c6123d3efb724c04caeccf1c0a
ys <719717747@qq.com>
2023-05-24 22:38:57 +0800

    111111111111111

--- chatgpt_module.py
+++ chatgpt_module.py
@@ -4,12 +4,12 @@ import os
 
 class ChatGPT:
     def __init__(self):
-        self.result = ""
         openai.api_key = os.environ.get('OPENAI_API_KEY')
         #
         # openai.proxy = "10.22.1.78:9999"
         # openai.proxy = "192.168.101.12:12307"
         self.history = []
+        self.Tokens = 0
 
     # 默认输入的信息中，包含场景，人物，动物，植物，颜色，建筑物那么我们将会直接提取然后出图
     import json
@@ -19,38 +19,20 @@ class ChatGPT:
         self.history.clear()
         return "clear"
 
-    def trim_history(history):
-        total_tokens = sum(len(message['content']) for message in history)
-        if total_tokens <= 4096:
-            return history
-
-        excess_tokens = total_tokens - 4096
-        tokens_to_remove = 0
-
-        # 递归移除历史记录中的旧内容，直到满足长度要求
-        for i, message in enumerate(history):
-            tokens_to_remove += len(message['content'])
-            if tokens_to_remove >= excess_tokens:
-                return history[i + 1:]
-
-        return history
-
     def request_gpt_quesion(self, user_Input):
-        self.history.append({"role": "user", "content": user_Input})
-        self.result = ""
-        self.result = "".join(str(value) for value in self.history)
+        self.history.append(f'user:{user_Input}')
+        self.Tokens += len(user_Input)
+        result = "".join(str(value) for value in self.history)
 
-        total_tokens = sum(len(message['content']) for message in self.history)
-        print(f'长度:{total_tokens}')
+        while self.Tokens >= 4000:
+            self.Tokens -= len(self.history[13])
+            self.history.pop(13)
 
-        while total_tokens >= 3500:
-            self.history.pop(len(self.history)-1)
-            total_tokens = sum(len(message['content']) for message in self.history)
-
-        self.result = "".join(str(value) for value in self.history)
+        print(f'Tokens:{self.Tokens}')
+        result2 = "".join(str(value) for value in self.history)
         response = openai.ChatCompletion.create(
             model="gpt-3.5-turbo",
-            messages=[{"role": "user", "content": self.result}]
+            messages=[{"role": "user", "content": result2}]
         )
 
         s = response['choices']
@@ -59,6 +41,6 @@ class ChatGPT:
         response_obj = s[0]
         # 获取 "content" 的值
         content = response_obj["message"]["content"]
-        self.history.append({"role": "ai", "content": content})
+        self.history.append(f'ai:{content}')
 
         return content
